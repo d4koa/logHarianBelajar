d37d2d552f83e0af18b4b563d524177244f90a1a
Dani Akhmad Maulana <121219043+d4koa@users.noreply.github.com>
2024-03-28 20:24:12 +0700

    RV_Algoritma.py
    Mereview materi algoritma dasar

---
+++ RV_Algoritma.py
@@ -0,0 +1,14 @@
+# Nama : Dani Akhmad Maulana  #
+# Perihal : Review Algoritma  #
+# Created at 28 Maret 2024    #
+
+## Strategi Algoritma
+# Algoritma -> serangkaian langkah-langkah tersusun atau sistematis yang memiliki tujuan untuk memecahkan suatu masalah.
+# Strategi -> siasat atau rencana dalam mencapai suatu tujuan.
+# Strategi Algoritma -> Siasat yang memiliki tujuan untuk memecahkan suatu persoalan dengan cara yang efektif, tersusun, dan cepat.
+
+## Bahasan
+# Runtunan -> sekumpulan baris kode yang dijalankan sekali jalan tanpa ada proses input 
+# Percabangan -> kode atau intruksi khusus yang dijalankan hanya ketika kondisi dari intruksi tersebut terpenuhi
+# Perulangan -> intruksi yang menjalankan satu atau lebih pernyataan sekaligus tergantung kondisi intruksi yang harus dipenuhi, jika sudah tidak memenuhi kondisi, maka perulangan selesai.
+# 
